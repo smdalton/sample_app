a06ddcec73039f5a5334761d8f5eba82a0c47e64
smd <shanemdalton@gmail.com>
2020-11-18 00:30:06 -0800

    added gravatar and sidebar

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get '/signup',  to: 'users#new'
   get '/login',   to: 'users#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+
 end
