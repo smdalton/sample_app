a514ada3e1737acd837750e5f1aa3bd999e74661
smd <shanemdalton@gmail.com>
2020-11-18 20:26:08 -0800

    added sign up features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,29 @@ class UsersController < ApplicationController
 
   def new
     # debugger
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    # when the post goes out to this route, we access the values for the new user
+    # object by accessing the instance variable returned as params[:user] which
+    # came from the new method above
+    # Rails takes the input from the form with name=user[email] and stores the email input into
+    # the hash called user which is made available via params above after receiving the post
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 
 
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,34 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test 'invald signup attempt' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '',
+                                         email: 'user@invalid',
+                                         password: '4444',
+                                         password_confirmation: '4441' } }
+    end
+    assert_template 'users/new'
+    assert_select 'div.field_with_errors'
+  end
+
+  test 'valid signup attempt' do
+    get signup_path
+    assert_difference 'User.count' do
+      post users_path, params: { user: { name: 'El Jefe',
+                                         email: 'user@valid.com',
+                                         password: '7777777',
+                                         password_confirmation: '7777777' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div.alert'
+  end
+
+
+
+end
