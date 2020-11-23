71fc19e600390850375cb05a12bfb5a807ccc6a5
smd <shanemdalton@gmail.com>
2020-11-23 00:42:15 -0800

    Implement basic login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,11 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def log_out
+    reset_session
+    @current_user = nil
+  end
+
   def current_user
     # current user equals itself if not nil, else hit the DB and get the session info
     @current_user ||= User.find_by(id: session[:user_id])
