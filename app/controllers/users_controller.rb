7b5bced1ccbed97c070bddf228398f3ce216dc08
smd <shanemdalton@gmail.com>
2020-12-07 15:29:17 -0800

    email config

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # @user.send_activation_email
-      @user.activate
+      @user.send_activation_email
+      # @user.activate
       flash[:info] = 'Please check your email to activate your account.'
       redirect_to root_url
     else
