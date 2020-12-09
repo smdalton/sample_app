82b32fadcb8238a790be91de836208455533471d
smd <shanemdalton@gmail.com>
2020-12-09 13:59:00 -0800

    removed email activation while waiting fo sendgrid

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -55,23 +55,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                        password: "password",
                                        password_confirmation: "password"}}
     end
-    assert_equal 1, ActionMailer::Base.deliveries.size
+    # assert_equal 1, ActionMailer::Base.deliveries.size
     user = assigns(:user)
-    assert_not user.activated?
+    # assert_not user.activated?
     # Try to log in before activation.
     log_in_as(user)
-    assert_not is_logged_in?
+    # assert_not is_logged_in?
     # Invalid activation token
-    get edit_account_activation_path("invalid token", email: user.email)
-    assert_not is_logged_in?
+    # get edit_account_activation_path("invalid token", email: user.email)
+    # assert_not is_logged_in?
     # Valid token, wrong email
-    get edit_account_activation_path(user.activation_token, email: 'wrong')
-    assert_not is_logged_in?
+    # get edit_account_activation_path(user.activation_token, email: 'wrong')
+    # assert_not is_logged_in?
     # Valid activation token
-    get edit_account_activation_path(user.activation_token, email: user.email)
-    assert user.reload.activated?
-    follow_redirect!
-    assert_template 'users/show'
+    # get edit_account_activation_path(user.activation_token, email: user.email)
+    # assert user.reload.activated?
+    # follow_redirect!
+    # assert_template 'users/show'
     assert is_logged_in?
   end
 end
